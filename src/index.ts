2c975c5c5340c35d34bcee728e40380240639e7f
Michael <michaelw@4sitestudios.com>
2024-11-27 12:50:52 -0500

    corrected the VGS option

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-//import { Options, App } from "@4site/engrid-scripts"; // Uses ENGrid via NPM
-import { Options, App } from "../../engrid/packages/scripts"; // Uses ENGrid via Visual Studio Workspace
+import { Options, App } from "@4site/engrid-scripts"; // Uses ENGrid via NPM
+//import { Options, App } from "../../engrid/packages/scripts"; // Uses ENGrid via Visual Studio Workspace
 
 import "./sass/main.scss";
 import { customScript } from "./scripts/main";
 
-const vgsFieldOpts = { css: {
+const vgsCss = {
   '@font-face': {
     'font-family': 'GraphikSemiBold',
     'src': `url('https://acb0a5d73b67fccd4bbe-c2d8138f0ea10a18dd4c43ec3aa4240a.ssl.cf5.rackcdn.com/10146/Graphik-Semibold-Web.woff')`,
     'font-display': 'swap'
   }
-}};
+};
 const options: Options = {
   applePay: false,
   CapitalizeFields: true,
@@ -23,9 +23,12 @@ const options: Options = {
   SrcDefer: true,
   ProgressBar: true,
   VGS: {
-    //"transaction.ccexpire": vgsFieldOpts,
-    "transaction.ccnumber": vgsFieldOpts,
-    "transaction.ccvv": vgsFieldOpts
+    "transaction.ccnumber": {
+      css: vgsCss      
+    },
+    "transaction.ccvv": {
+      css: vgsCss
+    }
   },
   Debug: App.getUrlParameter("debug") == "true" ? true : false,
   onLoad: () => customScript(),
