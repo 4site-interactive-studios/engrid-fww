e93a75297d40f5681be4480fd06c8c6da7c5b52d
bryancasler <bryan.casler@gmail.com>
2025-03-13 16:57:19 -0400

    Clean up

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Thursday, March 13, 2025 @ 15:48:29 ET
+ *  Date: Thursday, March 13, 2025 @ 16:56:29 ET
  *  By: bryancasler
  *  ENGrid styles: v0.20.9
  *  ENGrid scripts: v0.20.8
