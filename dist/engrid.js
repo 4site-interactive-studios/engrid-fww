9b508ad981948dfe0efa607dc84d89dfdcb4d97a
Daryl Brewer <daryl@zurigroup.com>
2025-04-15 17:58:40 -0400

    re-including original events code lost in merge

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,10 +17,10 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Tuesday, April 15, 2025 @ 14:14:09 ET
- *  By: bryancasler
- *  ENGrid styles: v0.21.0
- *  ENGrid scripts: v0.21.0
+ *  Date: Tuesday, April 15, 2025 @ 17:55:46 ET
+ *  By: daryl
+ *  ENGrid styles: v0.21.3
+ *  ENGrid scripts: v0.21.3
  *
  *  Created by 4Site Studios
  *  Come work with us or join our team, we would love to hear from you
@@ -11448,6 +11448,9 @@ class engrid_ENGrid {
                 case "emailsubscribeform":
                     return "SUBSCRIBEFORM";
                     break;
+                case "event":
+                    return "EVENT";
+                    break;
                 case "supporterhub":
                     return "SUPPORTERHUB";
                     break;
@@ -22266,7 +22269,7 @@ class PostDonationEmbed {
 }
 
 ;// CONCATENATED MODULE: ./node_modules/@4site/engrid-scripts/dist/version.js
-const AppVersion = "0.21.0";
+const AppVersion = "0.21.3";
 
 ;// CONCATENATED MODULE: ./node_modules/@4site/engrid-scripts/dist/index.js
  // Runs first so it can change the DOM markup before any markup dependent code fires
@@ -23386,6 +23389,9 @@ const customScript = function (App, EnForm) {
           optInCheckbox.checked = false;
         }
       });
+      donationInput.addEventListener("input", () => {
+        setTimeout(checkAmount, 50);
+      });
 
       // Also listen for change events (for autofill, etc.)
       mobilePhoneInput.addEventListener("change", function () {
@@ -23415,6 +23421,192 @@ const customScript = function (App, EnForm) {
     // );
     setupPhoneOptInCheckbox();
   }
+
+  // EVENT PAGE JS
+
+  // Allows ticket editor to make an unordered list with checkmark bullets by separating items with '@@'
+  function makeDescriptionLists() {
+    const descriptionLists = document.querySelectorAll(".en__ticket__desc");
+    descriptionLists.forEach(list => {
+      if (list.textContent.includes("@@")) {
+        const items = list.textContent.split("@@").map(item => item.trim()).filter(item => item);
+        const ul = document.createElement("ul");
+        ul.classList.add("checked-list");
+        items.forEach(item => {
+          const li = document.createElement("li");
+          li.innerHTML = `${item}`;
+          ul.appendChild(li);
+        });
+
+        // Replace the div content with the unordered list
+        list.innerHTML = "";
+        list.appendChild(ul);
+      }
+    });
+  }
+  makeDescriptionLists();
+
+  // Update the background color if the ticket amount is not 0
+  function activatedTicket() {
+    document.querySelectorAll("input.en__ticket__quantity").forEach(input => {
+      input.classList.add("is-zero");
+      const toggleClass = () => {
+        const inputText = input.value.trim();
+        const parentDiv = input.closest("div").parentElement.parentElement;
+        if (parentDiv) {
+          if (inputText !== "0" && inputText !== "") {
+            parentDiv.classList.add("activated");
+            input.classList.remove("is-zero");
+          } else {
+            parentDiv.classList.remove("activated");
+            input.classList.add("is-zero");
+          }
+        }
+      };
+      input.addEventListener("input", () => {
+        setTimeout(toggleClass, 50);
+      });
+      const plusDiv = input.parentElement.querySelector(".en__ticket__plus");
+      const minusDiv = input.parentElement.querySelector(".en__ticket__minus");
+      if (plusDiv) {
+        plusDiv.addEventListener("click", () => {
+          setTimeout(toggleClass, 50);
+        });
+      }
+      if (minusDiv) {
+        minusDiv.addEventListener("click", () => {
+          setTimeout(toggleClass, 50);
+        });
+      }
+    });
+  }
+  window.addEventListener("load", activatedTicket);
+
+  // Formatting for Additional Donation field
+  function additionalDonation() {
+    if (pageJson.pageType === "event" && pageJson.pageNumber == 1) {
+      const donationInput = document.querySelector("input.en__additional__input");
+      const checkAmount = () => {
+        const inputText = donationInput.value.trim();
+        const parentDiv = donationInput.closest("div").parentElement;
+        if (parentDiv) {
+          if (inputText !== "0" && inputText !== "") {
+            parentDiv.classList.add("activated");
+          } else {
+            parentDiv.classList.remove("activated");
+          }
+        }
+      };
+      donationInput.addEventListener("input", () => {
+        setTimeout(checkAmount, 50);
+      });
+    }
+  }
+  additionalDonation();
+
+  // Currency formatting
+  function formatCurrency(value) {
+    const numberValue = parseFloat(value.replace(/[^0-9.]/g, "")) || 0;
+    return `$${numberValue.toLocaleString("en-US", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    })}`;
+  }
+
+  // Apply currency formatting to Order Summary
+  function formatNumbers() {
+    if (pageJson.pageType === "event" && pageJson.pageNumber == 2) {
+      const summaryPrices = document.querySelectorAll(".en__orderSummary__data.en__orderSummary__data--cost");
+      summaryPrices.forEach(price => {
+        price.textContent = formatCurrency(price.textContent);
+      });
+    }
+  }
+  formatNumbers();
+
+  // Rounded-corners styling for Order Summary
+  function orderSummaryBorder() {
+    if (pageJson.pageType === "event" && pageJson.pageNumber == 2) {
+      const orderSummary = document.querySelector(".en__component.en__component--eventtickets");
+      if (orderSummary) {
+        orderSummary.classList.add("rounded-borders");
+      }
+    }
+  }
+  orderSummaryBorder();
+
+  // Create the rounded-corners container around the necessary content divs
+  const wrapElements = (startSelector, endSelector, wrapperClass = "border-container") => {
+    if (pageJson.pageType === "event" && pageJson.pageNumber == 2) {
+      const start = document.querySelector(startSelector);
+      const end = document.querySelector(endSelector);
+      if (start && end) {
+        const wrapper = document.createElement("div");
+        wrapper.classList.add(wrapperClass);
+        const fragment = document.createDocumentFragment();
+        start.appendChild(fragment);
+        let current = start.nextElementSibling;
+        while (current && current !== end) {
+          const next = current.nextElementSibling;
+          fragment.appendChild(current);
+          current = next;
+        }
+        wrapper.appendChild(fragment);
+        start.parentNode.insertBefore(wrapper, start.nextSibling);
+      } else {
+        console.error("Invalid start or end selector");
+      }
+    }
+  };
+  wrapElements(".billing-information-title", ".payment-information-title");
+  wrapElements(".payment-information-title", ".submit-button-container");
+
+  // Move title divs with the move-down class inside the container div below it
+  const moveDivInside = () => {
+    const divs = document.querySelectorAll(".move-down");
+    divs.forEach(div => {
+      const nextDiv = div.nextElementSibling;
+      if (nextDiv) {
+        nextDiv.insertBefore(div, nextDiv.firstChild);
+      } else {
+        console.warn("No div to move into for:", div);
+      }
+    });
+  };
+  setTimeout(moveDivInside, 200);
+  function orderSummaryDivider() {
+    if (pageJson.pageType === "event" && pageJson.pageNumber == 2) {
+      const targetDiv = document.querySelector(".en__orderSummary__headers");
+      if (targetDiv) {
+        newDiv.style.cssText = "display: table-row;";
+        newDiv.innerHTML = `
+          <div style="display:table-cell; border-bottom:1px solid #d8d8d8;"></div>
+          <div style="display:table-cell; border-bottom:1px solid #d8d8d8;"></div>
+          <div style="display:table-cell; border-bottom:1px solid #d8d8d8;"></div>
+          <div style="display:table-cell; border-bottom:1px solid #d8d8d8;"></div>
+        `;
+        targetDiv.after(newDiv);
+      }
+    }
+  }
+  setTimeout(orderSummaryDivider, 500);
+  function attendeePlaceholders() {
+    document.querySelectorAll('.en__registrants__registrant  label[for*="firstName"], .en__registrants__registrant label[for*="lastName"]').forEach(label => {
+      const input = label.nextElementSibling?.querySelector("input");
+      if (input) {
+        input.placeholder = `${label.textContent.trim()}*`;
+        label.style.display = "none";
+      }
+    });
+  }
+  //setTimeout(attendeePlaceholders, 1000);
+  attendeePlaceholders();
+  function hideAttendeeTitle() {
+    document.querySelectorAll(".en__registrants__registrantHead").forEach(title => {
+      title.classList.add("hide");
+    });
+  }
+  hideAttendeeTitle();
 };
 ;// CONCATENATED MODULE: ./src/index.ts
  // Uses ENGrid via NPM
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -152,7 +152,7 @@ export const customScript = function (App, EnForm) {
           // console.log("Setting mobile opt-in checkbox to unchecked");
           optInCheckbox.checked = false;
         }
-      };
+      });
 
       donationInput.addEventListener("input", () => {
         setTimeout(checkAmount, 50);
@@ -186,4 +186,232 @@ export const customScript = function (App, EnForm) {
     // );
     setupPhoneOptInCheckbox();
   }
+
+  // EVENT PAGE JS
+
+  // Allows ticket editor to make an unordered list with checkmark bullets by separating items with '@@'
+  function makeDescriptionLists() {
+    const descriptionLists = document.querySelectorAll(".en__ticket__desc");
+
+    descriptionLists.forEach((list) => {
+      if (list.textContent.includes("@@")) {
+        const items = list.textContent
+          .split("@@")
+          .map((item) => item.trim())
+          .filter((item) => item);
+
+        const ul = document.createElement("ul");
+        ul.classList.add("checked-list");
+
+        items.forEach((item) => {
+          const li = document.createElement("li");
+          li.innerHTML = `${item}`;
+          ul.appendChild(li);
+        });
+
+        // Replace the div content with the unordered list
+        list.innerHTML = "";
+        list.appendChild(ul);
+      }
+    });
+  }
+  makeDescriptionLists();
+
+  // Update the background color if the ticket amount is not 0
+  function activatedTicket() {
+    document.querySelectorAll("input.en__ticket__quantity").forEach((input) => {
+      input.classList.add("is-zero");
+      const toggleClass = () => {
+        const inputText = input.value.trim();
+        const parentDiv = input.closest("div").parentElement.parentElement;
+        if (parentDiv) {
+          if (inputText !== "0" && inputText !== "") {
+            parentDiv.classList.add("activated");
+            input.classList.remove("is-zero");
+          } else {
+            parentDiv.classList.remove("activated");
+            input.classList.add("is-zero");
+          }
+        }
+      };
+
+      input.addEventListener("input", () => {
+        setTimeout(toggleClass, 50);
+      });
+
+      const plusDiv = input.parentElement.querySelector(".en__ticket__plus");
+      const minusDiv = input.parentElement.querySelector(".en__ticket__minus");
+      if (plusDiv) {
+        plusDiv.addEventListener("click", () => {
+          setTimeout(toggleClass, 50);
+        });
+      }
+      if (minusDiv) {
+        minusDiv.addEventListener("click", () => {
+          setTimeout(toggleClass, 50);
+        });
+      }
+    });
+  }
+  window.addEventListener("load", activatedTicket);
+
+  // Formatting for Additional Donation field
+  function additionalDonation() {
+    if (pageJson.pageType === "event" && pageJson.pageNumber == 1) {
+      const donationInput = document.querySelector(
+        "input.en__additional__input"
+      );
+
+      const checkAmount = () => {
+        const inputText = donationInput.value.trim();
+        const parentDiv = donationInput.closest("div").parentElement;
+        if (parentDiv) {
+          if (inputText !== "0" && inputText !== "") {
+            parentDiv.classList.add("activated");
+          } else {
+            parentDiv.classList.remove("activated");
+          }
+        }
+      };
+
+      donationInput.addEventListener("input", () => {
+        setTimeout(checkAmount, 50);
+      });
+    }
+  }
+  additionalDonation();
+
+  // Currency formatting
+  function formatCurrency(value) {
+    const numberValue = parseFloat(value.replace(/[^0-9.]/g, "")) || 0;
+
+    return `$${numberValue.toLocaleString("en-US", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })}`;
+  }
+
+  // Apply currency formatting to Order Summary
+  function formatNumbers() {
+    if (pageJson.pageType === "event" && pageJson.pageNumber == 2) {
+      const summaryPrices = document.querySelectorAll(
+        ".en__orderSummary__data.en__orderSummary__data--cost"
+      );
+
+      summaryPrices.forEach((price) => {
+        price.textContent = formatCurrency(price.textContent);
+      });
+    }
+  }
+  formatNumbers();
+
+  // Rounded-corners styling for Order Summary
+  function orderSummaryBorder() {
+    if (pageJson.pageType === "event" && pageJson.pageNumber == 2) {
+      const orderSummary = document.querySelector(
+        ".en__component.en__component--eventtickets"
+      );
+      if (orderSummary) {
+        orderSummary.classList.add("rounded-borders");
+      }
+    }
+  }
+  orderSummaryBorder();
+
+  // Create the rounded-corners container around the necessary content divs
+  const wrapElements = (
+    startSelector,
+    endSelector,
+    wrapperClass = "border-container"
+  ) => {
+    if (pageJson.pageType === "event" && pageJson.pageNumber == 2) {
+      const start = document.querySelector(startSelector);
+      const end = document.querySelector(endSelector);
+
+      if (start && end) {
+        const wrapper = document.createElement("div");
+        wrapper.classList.add(wrapperClass);
+
+        const fragment = document.createDocumentFragment();
+        start.appendChild(fragment);
+
+        let current = start.nextElementSibling;
+
+        while (current && current !== end) {
+          const next = current.nextElementSibling;
+          fragment.appendChild(current);
+          current = next;
+        }
+
+        wrapper.appendChild(fragment);
+        start.parentNode.insertBefore(wrapper, start.nextSibling);
+      } else {
+        console.error("Invalid start or end selector");
+      }
+    }
+  };
+
+  wrapElements(".billing-information-title", ".payment-information-title");
+  wrapElements(".payment-information-title", ".submit-button-container");
+
+  // Move title divs with the move-down class inside the container div below it
+  const moveDivInside = () => {
+    const divs = document.querySelectorAll(".move-down");
+
+    divs.forEach((div) => {
+      const nextDiv = div.nextElementSibling;
+
+      if (nextDiv) {
+        nextDiv.insertBefore(div, nextDiv.firstChild);
+      } else {
+        console.warn("No div to move into for:", div);
+      }
+    });
+  };
+  setTimeout(moveDivInside, 200);
+
+  function orderSummaryDivider() {
+    if (pageJson.pageType === "event" && pageJson.pageNumber == 2) {
+      const targetDiv = document.querySelector(".en__orderSummary__headers");
+
+      if (targetDiv) {
+        newDiv.style.cssText = "display: table-row;";
+        newDiv.innerHTML = `
+          <div style="display:table-cell; border-bottom:1px solid #d8d8d8;"></div>
+          <div style="display:table-cell; border-bottom:1px solid #d8d8d8;"></div>
+          <div style="display:table-cell; border-bottom:1px solid #d8d8d8;"></div>
+          <div style="display:table-cell; border-bottom:1px solid #d8d8d8;"></div>
+        `;
+
+        targetDiv.after(newDiv);
+      }
+    }
+  }
+  setTimeout(orderSummaryDivider, 500);
+
+  function attendeePlaceholders() {
+    document
+      .querySelectorAll(
+        '.en__registrants__registrant  label[for*="firstName"], .en__registrants__registrant label[for*="lastName"]'
+      )
+      .forEach((label) => {
+        const input = label.nextElementSibling?.querySelector("input");
+
+        if (input) {
+          input.placeholder = `${label.textContent.trim()}*`;
+          label.style.display = "none";
+        }
+      });
+  }
+  //setTimeout(attendeePlaceholders, 1000);
+  attendeePlaceholders();
+
+  function hideAttendeeTitle() {
+    document
+      .querySelectorAll(".en__registrants__registrantHead")
+      .forEach((title) => {
+        title.classList.add("hide");
+      });
+  }
+  hideAttendeeTitle();
 };
