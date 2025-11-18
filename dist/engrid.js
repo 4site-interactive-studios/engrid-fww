d9b7b378478a7b3765aad46297d9c996222b7cab
cawecoy91 <cawecoy@gmail.com>
2025-11-18 01:32:24 -0300

    Fix embedded padding (4)

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Tuesday, November 18, 2025 @ 23:15:58 ET
+ *  Date: Tuesday, November 18, 2025 @ 23:32:10 ET
  *  By: cawe
  *  ENGrid styles: v0.23.4
  *  ENGrid scripts: v0.23.4
